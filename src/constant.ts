c9bd24788ab16e12e5e6b04d1a58af7ad09f50d1
chenliang <>
2023-11-29 13:37:51 +0800

    style: 🎨 update code format

--- src/constant.ts
+++ src/constant.ts
@@ -2,11 +2,11 @@ import {ResponseRedirectStatus} from "./typing";
 
 export const PROTOCOL = 'https';
 
+export const STATUS_CODE: ResponseRedirectStatus = 302;
+
 export const DOMAIN = "alphal.cn";
 
 export const PORT = 9527;
-
-export const STATUS_CODE: ResponseRedirectStatus = 302;
 
 export const SUBDOMAIN_WHITE_LIST = [
   'chat',
